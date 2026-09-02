46746fd740d650cf530edf8c3ccbccb767784c76
javeed122717 <jhakeem955@gmail.com>
2026-09-02 09:45:46 +0530

    Refactor variable assignments and clean up code

--- CIT_Student_Forecasting/cit_student_forecast/cit_forecast.py
+++ CIT_Student_Forecasting/cit_student_forecast/cit_forecast.py
@@ -2,20 +2,18 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
-data = pd.read_csv("cit_student_dataset.csv")
+data=pd.read_csv("cit_student_dataset.csv")
+train=data.tail(5)
+x=train[["year"]]
+y=train["students_joining"]
 
-# Use the latest five observations for a small, transparent trend model.
-train = data.tail(5)
-X = train[["year"]]
-y = train["students_joining"]
-
-model = LinearRegression()
+model=LinearRegression()
 model.fit(X, y)
 
-future_years = np.arange(2026, 2031)
-pred = np.rint(model.predict(future_years.reshape(-1, 1))).astype(int)
+future_years=np.arange(2026, 2031)
+pred=np.rint(model.predict(future_years.reshape(-1, 1))).astype(int)
 
-result = pd.DataFrame({
+result=pd.DataFrame({
     "academic_year": [f"{y}-{str(y+1)[-2:]}" for y in future_years],
     "predicted_students_joining": pred
 })
